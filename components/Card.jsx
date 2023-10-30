5b4a9851bd89d7af4a4725a1a7deece2ca7760b8
fariasagustin3 <fariasagustin3@gmail.com>
2023-10-30 16:45:20 -0300

    first commit

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,14 +1,38 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text, Image } from 'react-native';
 
-const Card = () => {
+const Card = ({ image, title, seasons, currentSeason, currentChapter, status }) => {
   return (
-    <View>
-      <Text>Card Component</Text>
+    <View style={styles.container}>
+      <Image
+        source={{ uri: image }}
+        style={{ width: 150, height: 200, borderRadius: 5 }}
+      />
+      <Text style={styles.title}>{title}</Text>
+      <Text style={status === "Activo" ? styles.activeStatus : styles.inactiveSatus}>{status}</Text>
+      <Text>Temporada: {currentSeason}</Text>
+      <Text>Capitulo: {currentChapter}</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+
+  },
+  title: {
+    color: 'black',
+    fontSize: 14,
+    fontWeight: 'bold',
+    width: 140, 
+    textAlign: 'center'
+  },
+  activeStatus: {
+
+  },
+  inactiveSatus: {
+
+  }
+})
 
 export default Card;
--- components/SeriesList.jsx
+++ components/SeriesList.jsx
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {View, StyleSheet, Text} from 'react-native';
+import Card from './Card';
+import axios from 'axios';
 
-const SeriesList = () => {
+const SeriesList = ({ active, inactive }) => {
+  const [activeSeries, setActiveSeries] = useState([]);
+  const [inactiveSeries, setInactiveSeries] = useState([]);
+
+  useEffect(() => {
+    const getAllSeries = async () => {
+      const seriesActive = await axios.get('https://series-tracker.onrender.com/series/active/list');
+      const seriesInactive = await axios.get('https://series-tracker.onrender.com/series/inactive/list');
+      setActiveSeries(seriesActive);
+      setInactiveSeries(seriesInactive);
+
+    }
+
+    getAllSeries();
+  })
+
   return (
     <View>
-      <Text>Series List</Text>
+      {(active && activeSeries) && activeSeries?.map((s) => (
+        <Card
+          key={s.id}
+          image={s.poster}
+          title={s.title}
+          currentSeason={s.currentSeason}
+          currentChapter={s.currentChapter}
+          status={s.status}
+        />
+      ))}
+      {(inactive && inactiveSeries) && inactiveSeries?.map((s) => (
+        <Card
+          key={s.id}
+          image={s.poster}
+          title={s.title}
+          currentSeason={s.currentSeason}
+          currentChapter={s.currentChapter}
+          status={s.status}
+        />
+      ))}
     </View>
   );
 }
